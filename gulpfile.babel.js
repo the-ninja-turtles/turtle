daeb6c575147a3259566e18e6c3d2c2b95af44cb
David Craven <david@craven.ch>
2015-08-27 18:01:11 -0700

    (fix) Update linting task for new directory structure

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,7 @@ let gulp = require('gulp');
 let eslint = require('gulp-eslint');
 
 gulp.task('lint', () => {
-  return gulp.src(['client/**/*.js'])
+  return gulp.src(['**/*.js', '!**/config.js', '!**/node_modules/**/*.js', '!**/jspm_packages/**/*.js'])
     .pipe(eslint({useEslintrc: true}))
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
