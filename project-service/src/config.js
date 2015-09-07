f08891a7d096594a65f8f2494bc7cc536384fd31
Wesley Tsai <wezley.tsai@gmail.com>
2015-09-07 15:34:17 -0700

    (fix) Remove "development" db configuration
    - No longer needed since docker  will instantiate separate instance of postgres for development

--- project-service/src/config.js
+++ project-service/src/config.js
@@ -1,11 +1,4 @@
 let config = {
-  development: {
-    host: '127.0.0.1',
-    dialect: 'postgres',
-    database: 'turtle',
-    username: 'test',
-    password: ''
-  },
   test: {
     host: '127.0.0.1',
     dialect: 'sqlite',
